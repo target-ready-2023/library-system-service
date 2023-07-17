0b0b7401701cd5287117367d3283774ab88dd49d
Jahnavi8122 <jahnavipabbathi812@gmail.com>
2023-07-17 19:17:24 +0530

    Integrating both the applications done

--- LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/Repository/BookRepository.java
+++ LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/Repository/BookRepository.java
@@ -1,9 +1,14 @@
 package com.target.ready.library.system.service.LibrarySystemService.Repository;
 
 import com.target.ready.library.system.service.LibrarySystemService.Entity.Book;
+import com.target.ready.library.system.service.LibrarySystemService.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    public List<Book> findAll();
+
 }
