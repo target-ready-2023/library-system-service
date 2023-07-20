cc2f02aa69cadfc8ad836d1b4e5b46ea4b3b7d05
Pavan Kalyan T <tpavankalyan18@gmail.com>
2023-07-20 20:45:55 +0530

    update book api added

---
+++ LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.target.ready.library.system.service.LibrarySystemService.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
