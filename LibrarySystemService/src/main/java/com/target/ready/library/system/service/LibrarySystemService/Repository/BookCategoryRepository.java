e9aa259f423ca4f4ec03950d515bf2b059143249
surajsasikumar <surajsasilko@gmail.com>
2023-07-25 00:20:27 +0530

    Remove unused import

--- LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/Repository/BookCategoryRepository.java
+++ LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/Repository/BookCategoryRepository.java
@@ -1,7 +1,6 @@
 package com.target.ready.library.system.service.LibrarySystemService.Repository;
 
 import com.target.ready.library.system.service.LibrarySystemService.Entity.BookCategory;
-import com.target.ready.library.system.service.LibrarySystemService.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
