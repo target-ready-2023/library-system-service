02c9f781e38e44befecad0776b919e92c288c185
surajsasikumar <surajsasilko@gmail.com>
2023-08-01 23:00:16 +0530

    Delete Test

--- LibrarySystemService/src/test/java/com/target/ready/library/system/service/LibrarySystemService/service/CategoryServiceTest.java
+++ LibrarySystemService/src/test/java/com/target/ready/library/system/service/LibrarySystemService/service/CategoryServiceTest.java
@@ -2,11 +2,19 @@ package com.target.ready.library.system.service.LibrarySystemService.service;
 
 
 import com.target.ready.library.system.service.LibrarySystemService.controller.LibraryControllerTest;
+import com.target.ready.library.system.service.LibrarySystemService.entity.BookCategory;
 import com.target.ready.library.system.service.LibrarySystemService.repository.BookCategoryRepository;
 import com.target.ready.library.system.service.LibrarySystemService.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doAnswer;
 
 @SpringBootTest(classes = {CategoryServiceTest.class})
 public class CategoryServiceTest {
@@ -19,4 +27,30 @@ public class CategoryServiceTest {
 
     @InjectMocks
     CategoryService categoryService;
+
+    @Test
+    public void deleteBookCategoryTest()
+    {
+        List<BookCategory> categories = new ArrayList<>();
+        BookCategory category1= new BookCategory();
+        category1.setId(3);
+        category1.setBookId(1);
+        category1.setCategoryName("Thriller");
+        categories.add(category1);
+
+        BookCategory category2= new BookCategory();
+        category2.setId(4);
+        category2.setBookId(2);
+        category2.setCategoryName("Suspense");
+        categories.add(category2);
+
+        doAnswer((i) -> {
+            categories.removeIf(category -> category.getBookId() == 1);
+            return null;
+        }).when(bookCategoryRepository).deleteById(1);
+        categoryService.deleteBookCategory(1);
+
+        assertEquals(categories.size(),1);
+
+    }
 }
--- LibrarySystemService/src/test/java/com/target/ready/library/system/service/LibrarySystemService/service/LibrarySystemServiceTest.java
+++ LibrarySystemService/src/test/java/com/target/ready/library/system/service/LibrarySystemService/service/LibrarySystemServiceTest.java
@@ -2,11 +2,19 @@ package com.target.ready.library.system.service.LibrarySystemService.service;
 
 
 import com.target.ready.library.system.service.LibrarySystemService.controller.LibraryControllerTest;
+import com.target.ready.library.system.service.LibrarySystemService.entity.Book;
 import com.target.ready.library.system.service.LibrarySystemService.repository.BookCategoryRepository;
 import com.target.ready.library.system.service.LibrarySystemService.repository.BookRepository;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doAnswer;
 
 @SpringBootTest(classes = {LibrarySystemServiceTest.class})
 public class LibrarySystemServiceTest {
@@ -19,4 +27,37 @@ public class LibrarySystemServiceTest {
 
     @InjectMocks
     LibrarySystemService librarySystemService;
+
+    @Test
+    public void deleteBookTest() {
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book();
+        book1.setBookId(1);
+        book1.setBookName("Life of Suraj 1");
+        book1.setBookDescription("Masterpiece");
+        book1.setAuthorName("Suraj");
+        book1.setPublicationYear(2024);
+        books.add(book1);
+
+
+        Book book2 = new Book();
+        book2.setBookId(2);
+        book2.setBookName("Life of Suraj 2");
+        book2.setBookDescription("Masterpiece");
+        book2.setAuthorName("Suraj");
+        book2.setPublicationYear(2024);
+        books.add(book2);
+
+
+        doAnswer((invocation) -> {
+            int id=invocation.getArgument(0);
+            books.removeIf(book->book.getBookId()==id);
+            return null;
+        }).when(bookRepository).deleteById(1);
+        
+        librarySystemService.deleteBook(1);
+        assertEquals(books.size(),1);
+    }
+
 }
