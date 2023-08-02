b946a62813baf18e5ae4e855f01db294c365c88c
Shubhani25 <shubhanimulay25@gmail.com>
2023-08-02 11:28:34 +0530

    Changed folder structure

--- LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.target.ready.library.system.service.LibrarySystemService.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-
-}
--- LibrarySystemService/src/main/java/com/target/ready/library/system/service/LibrarySystemService/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.target.ready.library.system.service.LibrarySystemService.repository;
-
-import com.target.ready.library.system.service.LibrarySystemService.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
